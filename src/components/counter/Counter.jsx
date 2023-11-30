a2c91c474840b9de3f216702c37357b90db2a9fc
frexit <fr3xit@gmail.com>
2023-11-30 13:46:31 +0300

    30-11-2023:13:46

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -8,13 +8,17 @@ function Counter({count, onPress}) {
   const styles = useStyleCounter();
   return (
     <View style={styles.box}>
-      <TouchableOpacity style={styles.button} onPress={onPress}>
+      <TouchableOpacity
+        style={[styles.increment, styles.button]}
+        onPress={onPress}>
         <FontAwesomeIcon icon="minus" />
       </TouchableOpacity>
 
       <Text>{count}</Text>
 
-      <TouchableOpacity style={styles.button} onPress={onPress}>
+      <TouchableOpacity
+        style={[styles.decrement, styles.button]}
+        onPress={onPress}>
         <FontAwesomeIcon icon="plus" />
       </TouchableOpacity>
     </View>
--- src/components/counter/useStyleCounter.js
+++ src/components/counter/useStyleCounter.js
@@ -4,14 +4,28 @@ import COLORS from '../../assets/styles/stylesColors';
 function useStyleCounter() {
   const styles = StyleSheet.create({
     box: {
-      justifyContent: 'center',
-      paddingVertical: 4,
-      paddingHorizontal: 8,
+      width: 200,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+
       borderRadius: 50,
       borderWidth: 1,
       borderColor: COLORS.black,
       backgroundColor: COLORS.accent,
       elevation: 7,
+    },
+
+    button: {
+      paddingHorizontal: 8,
+      borderColor: COLORS.black,
+    },
+
+    increment: {
+      borderRightWidth: 1,
+    },
+
+    decrement: {
+      borderLeftWidth: 1,
     },
   });
 
