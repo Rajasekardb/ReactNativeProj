29816e02ee7c51f446382e9aead81021a1721548
frexit <fr3xit@gmail.com>
2023-11-30 11:43:11 +0300

    30-11-2023:11:43

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 
 import {ROUTES_DATA} from '../../routes/routes-data';
 import ButtonIcon from '../buttonIcon/ButtonIcon';
@@ -10,14 +10,25 @@ import useStyleHeader from './useStyleHeader';
 function Header({title}) {
   const styles = useStyleHeader();
   const navigation = useNavigation();
+  const route = useRoute();
   const goBack = () => navigation.goBack();
   const goMenu = () => navigation.push(ROUTES_DATA.home.name);
-
+  const goBasket = () => navigation.push(ROUTES_DATA.basket.name);
+  const isShowBasket = route.name !== ROUTES_DATA.basket.name;
   return (
     <View style={styles.header}>
-      <ButtonIcon onPress={goBack} iconName="arrow-left" />
+      <View style={styles.leftHub}>
+        <ButtonIcon onPress={goMenu} iconName="bars" />
+        <ButtonIcon onPress={goBack} iconName="arrow-left" />
+      </View>
+
       <Text style={styles.title}>{title}</Text>
-      <ButtonIcon onPress={goMenu} iconName="bars" />
+
+      <View style={styles.rightHub}>
+        {isShowBasket && (
+          <ButtonIcon onPress={goBasket} iconName="basket-shopping" />
+        )}
+      </View>
     </View>
   );
 }
