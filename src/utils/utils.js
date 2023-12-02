ded87e9412fae05aea635ea606859a9efd25eaee
frexit <fr3xit@gmail.com>
2023-12-02 08:17:44 +0300

    02-12-2023:08:17

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,12 @@
 function generateId() {
   const data = new Date().getTime();
   const random = Math.random();
+
   return `id_${String(data)}_${random}`;
 }
 
-export {generateId};
+function generateRandomNumber(min = 0, max = 10) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export {generateId, generateRandomNumber};
